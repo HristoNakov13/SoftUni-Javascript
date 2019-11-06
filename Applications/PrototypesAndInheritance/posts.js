aee62c6245a47c7725d9dc4944588af7799981d4
HristoNakov13 <admiralbiat@gmail.com>
2019-11-06 12:38:51 +0200

    exercise

---
+++ Applications/PrototypesAndInheritance/posts.js
@@ -0,0 +1,59 @@
+function posts() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`;
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            return super.toString() + `\nRating: ${this.likes - this.dislikes}${this.getComments()}`;
+        }
+
+        getComments() {
+            if (this.comments.length === 0) {
+                return "";
+            }
+
+            return `\nComments:\n * ${this.comments.join("\n * ")}`;
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`;
+        }
+    }
+
+    return {
+        Post,
+        BlogPost,
+        SocialMediaPost,
+    }
+}
